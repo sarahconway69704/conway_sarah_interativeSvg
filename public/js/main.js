24d0d68a55253f68b7f801d218a742e58f96b6e0
Sarah Conway <sarahconway@Sarahs-MacBook-Pro.local>
2019-11-30 23:40:08 -0500

    got rid of hover dots, tried my best.

--- public/js/main.js
+++ public/js/main.js
@@ -5,34 +5,33 @@
     const infoButton = document.querySelectorAll('.info');
     shimmers = document.querySelector('.sparkle');
     
-    const hmatte1 = document.getElementById("m-top-left");
-    const hmatte2 = document.getElementById("m-top-right");
-    const hmatte3 = document.getElementById("m-mid-left");
-    const hmatte4 = document.getElementById("m-mid-right");
-    const hmatte5 = document.getElementById("m-bot-mid");
+    // const hmatte2 = document.getElementById("m-top-right");
+    // const hmatte3 = document.getElementById("m-mid-left");
+    // const hmatte4 = document.getElementById("m-mid-right");
+    // const hmatte5 = document.getElementById("m-bot-mid");
 
-    const hshimmer1 = document.getElementById("s-top-mid");
-    const hshimmer2 = document.getElementById("s-mid-mid");
-    const hshimmer3 = document.getElementById("s-bot-left");
-    const hshimmer4 = document.getElementById("s-bot-right");
+    // const hshimmer1 = document.getElementById("s-top-mid");
+    // const hshimmer2 = document.getElementById("s-mid-mid");
+    // const hshimmer3 = document.getElementById("s-bot-left");
+    // const hshimmer4 = document.getElementById("s-bot-right");
 
-    const hudaLocations = 
-    [
-      {
-        hmatte1: { topx: 100, topy: 100},
-        hmatte2: { topx: 300, topy: 300},
-        hmatte3: { topx: 400, topy: 400},
-        hmatte4: { topx: 500, topy: 500},
-        hmatte5: { topx: 600, topy: 600},
-      }, 
+    // const hudaLocations = 
+    // [
+    //   {
+    //     hmatte1: { topx: 100, topy: 100},
+    //     hmatte2: { topx: 300, topy: 300},
+    //     hmatte3: { topx: 400, topy: 400},
+    //     hmatte4: { topx: 500, topy: 500},
+    //     hmatte5: { topx: 600, topy: 600},
+    //   }, 
 
-      {
-        hshimmer1: { topx: 100, topy: 100},
-        hshimmer2: { topx: 300, topy: 300},
-        hshimmer3: { topx: 400, topy: 400},
-        hshimmer4: { topx: 500, topy: 500},
-      }
-    ]
+    //   {
+    //     hshimmer1: { topx: 100, topy: 100},
+    //     hshimmer2: { topx: 300, topy: 300},
+    //     hshimmer3: { topx: 400, topy: 400},
+    //     hshimmer4: { topx: 500, topy: 500},
+    //   }
+    // ]
 
 
     function buildPopover(makeup_stats, el) {
@@ -76,28 +75,28 @@
      }
 
 
-function mouseOver() {
-  mattes.classList.add('show-solids');
-}
+// function mouseOver() {
+//   mattes.classList.add('show-solids');
+// }
 
-function mouseOut() {
-  mattes.classList.remove('show-solids');
-}
+// function mouseOut() {
+//   mattes.classList.remove('show-solids');
+// }
 
-function mouseOver2() {
-  shimmers.classList.add('show-sparkle');
-}
+// function mouseOver2() {
+//   shimmers.classList.add('show-sparkle');
+// }
 
-function mouseOut2() {
-  shimmers.classList.remove('show-sparkle');
-}
+// function mouseOut2() {
+//   shimmers.classList.remove('show-sparkle');
+// }
 
   
       popOver.addEventListener("click", toggle),
-      infoButton.forEach(button => button.addEventListener("click", fetchData)),
-      document.getElementById("mats1").addEventListener("mouseover", mouseOver),
-      document.getElementById("mats1").addEventListener("mouseout", mouseOut),
-      document.getElementById("shim1").addEventListener("mouseover", mouseOver2),
-      document.getElementById("shim1").addEventListener("mouseout", mouseOut2);
+      infoButton.forEach(button => button.addEventListener("click", fetchData));
+      // document.getElementById("mats1").addEventListener("mouseover", mouseOver),
+      // document.getElementById("mats1").addEventListener("mouseout", mouseOut),
+      // document.getElementById("shim1").addEventListener("mouseover", mouseOver2),
+      // document.getElementById("shim1").addEventListener("mouseout", mouseOut2);
 
 })();
