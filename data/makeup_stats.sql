0500860215a263402e59e456468edf8dde76da96
Sarah Conway <sarahconway@Sarahs-MacBook-Pro.local>
2019-12-02 16:32:45 -0500

    data updated

--- data/makeup_stats.sql
+++ data/makeup_stats.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 24, 2019 at 02:36 AM
+-- Generation Time: Dec 02, 2019 at 05:41 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.8
 
@@ -34,21 +34,21 @@ CREATE TABLE `makeup_stats` (
   `mattes` int(10) NOT NULL,
   `shimmers` int(10) NOT NULL,
   `reviews` int(10) NOT NULL,
-  `5-stars` int(10) NOT NULL,
-  `4-stars` int(10) NOT NULL,
-  `3-stars` int(10) NOT NULL,
-  `2-stars` int(10) NOT NULL,
-  `1-star` int(10) NOT NULL,
-  `retail-price` varchar(10) NOT NULL,
-  `avg-rating` varchar(10) NOT NULL,
-  `image-palette` varchar(50) NOT NULL
+  `fivestars` int(10) NOT NULL,
+  `fourstars` int(10) NOT NULL,
+  `threestars` int(10) NOT NULL,
+  `twostars` int(10) NOT NULL,
+  `onestar` int(10) NOT NULL,
+  `retailprice` varchar(10) NOT NULL,
+  `avgrating` varchar(10) NOT NULL,
+  `imagepalette` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `makeup_stats`
 --
 
-INSERT INTO `makeup_stats` (`ID`, `company_name`, `palette_name`, `colours`, `mattes`, `shimmers`, `reviews`, `5-stars`, `4-stars`, `3-stars`, `2-stars`, `1-star`, `retail-price`, `avg-rating`, `image-palette`) VALUES
+INSERT INTO `makeup_stats` (`ID`, `company_name`, `palette_name`, `colours`, `mattes`, `shimmers`, `reviews`, `fivestars`, `fourstars`, `threestars`, `twostars`, `onestar`, `retailprice`, `avgrating`, `imagepalette`) VALUES
 (1, 'Huda Beauty', 'Nude Obessions', 9, 5, 4, 64, 46, 6, 5, 4, 3, '$34', '4.4', 'huda_palette.svg'),
 (2, 'Natasha Denona', 'Sunset', 15, 9, 6, 338, 259, 29, 25, 18, 8, '$119', '4.5', 'sunset_palette.svg'),
 (3, 'Urban Decay', 'Born to Run', 21, 11, 10, 856, 614, 131, 39, 33, 39, '$65', '4.5', 'urban_palette.svg');
